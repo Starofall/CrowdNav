c486fec63da3cee1d1e13138fc4330db7ff465ad
Ilias Gerostathopoulos <iliasger@gmail.com>
2017-12-12 13:56:59 +0100

    removing code related to explicit routes

--- app/entitiy/Car.py
+++ app/entitiy/Car.py
@@ -49,12 +49,6 @@ class Car:
         # number of ticks since last reroute / arrival
         self.lastRerouteCounter = 0
 
-        # just to make the setup less random
-        self.explicitRoute = id <= float(Config.smartCarCounter) / 2
-        self.target1 = "64"
-        self.target2 = "2284"
-        self.isTarget1 = True
-
     def setArrived(self, tick):
         """ car arrived at its target, so we add some statistic data """
 
@@ -107,16 +101,7 @@ class Car:
         else:
             self.sourceID = self.targetID  # We start where we stopped
 
-        if self.smartCar and self.explicitRoute:
-            if self.isTarget1:
-                self.targetID = self.target1
-                self.isTarget1 = False
-            else:
-                self.targetID = self.target2
-                self.isTarget1 = True
-        else:
-            self.targetID = random.choice(Network.nodes).getID()
-
+        self.targetID = random.choice(Network.nodes).getID()
         self.currentRouteID = self.id + "-" + str(self.rounds)
         self.currentRouterResult = CustomRouter.route(self.sourceID, self.targetID, tick, self)
         if len(self.currentRouterResult.route) > 0:
